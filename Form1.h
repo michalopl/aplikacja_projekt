01427ff9fb8d8f88f905e1c368d069a750a3a875
michalopl <2b6wad1w9iz.>
2015-06-05 15:29:32 +0200

    das

--- Form1.h
+++ Form1.h
@@ -61,6 +61,7 @@ namespace WindowsFormApplication1 {
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// Form1
 			// 
@@ -74,6 +75,8 @@ namespace WindowsFormApplication1 {
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
 
